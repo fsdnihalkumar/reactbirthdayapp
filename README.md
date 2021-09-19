1654e31949033fe4662af5eebe2170fbbf5dc7fd
Your Name <you@example.com>
2021-09-19 10:55:44 +0530

    1st Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This project is designed by Nihal Kumar. 
+
+
+
+
+This is just for the practice purpose.  I have planned to make 100 mini react projects
+
+
+This is one step towards that.
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Nihal Kumar',
+      age: 23,
+      image:
+        'https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    },
+    {
+      id: 2,
+      name: 'Debashan China',
+      age: 22,
+      image:
+        'https://images.pexels.com/photos/1933873/pexels-photo-1933873.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    },
+    {
+      id: 3,
+      name: 'Fahad',
+      age: 21,
+      image:
+        'https://images.pexels.com/photos/3170635/pexels-photo-3170635.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+    },
+    {
+      id: 4,
+      name: 'Garima ',
+      age: 24,
+      image:
+        'https://images.pexels.com/photos/4048497/pexels-photo-4048497.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+    },
+    {
+      id: 5,
+      name: 'Raju Singh',
+      age: 19,
+      image:
+        'https://images.pexels.com/photos/2896853/pexels-photo-2896853.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
+    },
+  ];
